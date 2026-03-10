adbc0718d741b9f8b7796bc214b7d81bc3515007
Weon_hyeok2 <kwh102455@gmail.com>
2026-03-10 15:49:14 +0900

    MainActivity 리팩토링

--- week7/MainActivity.kt
+++ week7/MainActivity.kt
@@ -1,55 +1,71 @@
-package com.practice.stop_watch
+package com.practice.androidbookpractice
 
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import com.practice.stop_watch.databinding.ActivityMainBinding
+import com.practice.androidbookpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var initTime = 0L
     var pauseTime = 0L
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.startButton.setOnClickListener {
-            binding.timer.base = SystemClock.elapsedRealtime() + pauseTime
-            binding.timer.start()
+        setUpListener()
 
-            binding.startButton.isEnabled = false
-            binding.stopButton.isEnabled = true
-            binding.resetButton.isEnabled = true
+        setupBackClick()
+
+    }
+
+
+    private fun setUpListener() {
+        with(binding) {
+            startButton.setOnClickListener {
+                timer.base = SystemClock.elapsedRealtime() - pauseTime
+                timer.start()
+                changeButtonEnableStatus(startButton.isEnabled)
+            }
+            stopButton.setOnClickListener {
+                pauseTime = SystemClock.elapsedRealtime() - timer.base
+                timer.stop()
+                changeButtonEnableStatus(startButton.isEnabled)
+            }
+            resetButton.setOnClickListener {
+                timer.base = SystemClock.elapsedRealtime()
+                pauseTime = 0L
+                timer.stop()
+                changeButtonEnableStatus(startButton.isEnabled, resetBtn = true)
+            }
         }
+    }
 
-        binding.stopButton.setOnClickListener {
-            pauseTime = binding.timer.base - SystemClock.elapsedRealtime()
-            binding.timer.stop()
-
-            binding.startButton.isEnabled = true
-            binding.stopButton.isEnabled = false
-            binding.resetButton.isEnabled = true
+    private fun changeButtonEnableStatus(startBtn: Boolean, resetBtn: Boolean = false) {
+        with(binding) {
+            startButton.isEnabled = !startBtn
+            stopButton.isEnabled = startBtn
+            resetButton.isEnabled = startBtn || pauseTime > 0L
+            if (resetBtn) resetButton.isEnabled = false
         }
+    }
 
-        binding.resetButton.setOnClickListener {
-            pauseTime = 0L
-            binding.timer.base = SystemClock.elapsedRealtime()
-            binding.timer.stop()
-            binding.startButton.isEnabled = true
-            binding.stopButton.isEnabled = false
-            binding.resetButton.isEnabled = false
-        }
-
-        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
+    private fun setupBackClick(): Unit {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (System.currentTimeMillis() - initTime >= 3000) {
                     initTime = System.currentTimeMillis()
-                    Toast.makeText(this@MainActivity, "종료하려면 한 번 더 누르세요", Toast.LENGTH_SHORT).show()
+                    toast?.cancel()
+                    toast = Toast.makeText(application, "종료하려면 한 번 더 눌러주세요", Toast.LENGTH_SHORT)
+                    toast?.show()
                 } else {
+                    toast?.cancel()
                     finish()
                 }
             }
